e9e0b42ee001583fb1aae428b3478b640466f44a
Steve King, Jr <stevekx86@gmail.com>
2015-06-08 19:44:36 -0700

    Added parent reference to node. Makes things a little easier.

--- binary_tree/java/src/Node.java
+++ binary_tree/java/src/Node.java
@@ -1,6 +1,7 @@
 public class Node<T extends Comparable<T>> {
 
     private T value;
+    private Node<T> parent;
     private Node<T> left;
     private Node<T> right;
 
@@ -10,10 +11,15 @@ public class Node<T extends Comparable<T>> {
         right = null;
     }
 
-    public Node(T value, Node<T> left, Node<T> right) {
+    public Node(T value, Node<T> parent, Node<T> left, Node<T> right) {
         this.value = value;
+        this.parent = parent;
         this.left = left;
         this.right = right;
+    }
+
+    public void setParent(Node<T> node) {
+        this.parent = node;
     }
 
     public void setLeft(Node<T> node) {
@@ -22,6 +28,10 @@ public class Node<T extends Comparable<T>> {
 
     public void setRight(Node<T> node) {
         this.right = node;
+    }
+
+    public Node<T> getParent() {
+        return parent;
     }
 
     public Node<T> getLeft() {
