4622a643ac705f7c1e3341ef066c8cdd8e923c8d
Steve King, Jr <stevekx86@gmail.com>
2015-06-08 19:13:44 -0700

    More accessors and setters.

--- binary_tree/java/src/Node.java
+++ binary_tree/java/src/Node.java
@@ -6,6 +6,14 @@ public class Node<T extends Comparable<T>> {
 
     public Node(T value) {
         this.value = value;
+        left = null;
+        right = null;
+    }
+
+    public Node(T value, Node<T> left, Node<T> right) {
+        this.value = value;
+        this.left = left;
+        this.right = right;
     }
 
     public void setLeft(Node<T> node) {
@@ -14,6 +22,18 @@ public class Node<T extends Comparable<T>> {
 
     public void setRight(Node<T> node) {
         this.right = node;
+    }
+
+    public Node<T> getLeft() {
+        return left;
+    }
+
+    public Node<T> getRight() {
+        return right;
+    }
+
+    public T getValue() {
+        return value;
     }
 
 }
